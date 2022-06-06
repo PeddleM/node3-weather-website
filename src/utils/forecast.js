62e9b7e4b4900f6d04074115ece53221989c4a62
Michael Peddle <michaelp@iris.net>
2022-06-06 18:40:23 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    
+    const url = 'http://api.weatherstack.com/current?access_key=fd016abe5899f17233b40d165fff358d&query=' + latitude + ',' + longitude + '&units=m'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (error) {
+            callback('Unable to find location', undefined)
+        } else {
+
+            const summary = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
+                ' degrees out, and feels like ' + body.current.feelslike + '.'
+
+            callback(undefined, summary)
+        }  
+    })
+}
+
+module.exports = forecast
